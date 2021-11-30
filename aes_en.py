82f4d2da10a3ca8d26f95a6b9d40880b83cb8d31
ZuZa1235012350 <80256178+ZuZa1235012350@users.noreply.github.com>
2021-11-30 20:05:57 +0100

    Update aes_en.py

--- aes_en.py
+++ aes_en.py
@@ -23,12 +23,12 @@ while True:
     if choice == '3':
         exit(0)
     if choice == '1':
-        for file in glob.glob("/Users/Alex/Desktop/BSI2/PY/TEXTVIRUS310/Encrypt_descrypt/test/*.txt"):
+        for file in glob.glob('test/*.txt'):
             encrypt(key, file)
             print("done")
     # for file in glob.glob("files/*.txt"):
     #     os.remove(file)
     if choice == '2':
-        for file in glob.glob('/Users/Alex/Desktop/BSI2/PY/TEXTVIRUS310/Encrypt_descrypt/test/*.crypted'):
+        for file in glob.glob('test/*.crypted'):
             decrypt(key, file)
             print("done")
